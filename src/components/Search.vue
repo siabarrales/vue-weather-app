e50963b190c7c3a3ec530892cf9710b59c251d50
Sebastian Avendaño Barrales <sebastian.avendano@borealis.cl>
2022-11-05 23:19:24 -0300

    feature: Search component design

---
+++ src/components/Search.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <h1>Weather App</h1>
+    <div class="search-container">
+      <input type="text" placeholder="Enter a city" />
+      <button class="btn-search">Search</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: 20px;
+}
+.search-container {
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.btn-search {
+  margin-left: 10px;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="card">
+    <h1>Weather Content</h1>
+  </div>
+</template>
+
+<style scoped></style>
